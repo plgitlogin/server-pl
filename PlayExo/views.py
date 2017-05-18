10c3b948b0b8a60d91cda5aa15b3540517f7ce52
dr <dr@pl-sandbox-test.u-pem.fr>
2017-05-18 17:03:18 +0200

    remove Loadded

--- PlayExo/views.py
+++ PlayExo/views.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env/ python3
 # coding: utf-8
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 from serverpl.settings import MEDIA_ROOT
 
@@ -16,9 +18,9 @@ logger = logging.getLogger(__name__)
 
 
 def pl_view(request, pltp_name, pl_name):
-    current_tp = get_object_or_404(Loaded_Pltp, name=pltp_name)
-    current_pl = get_object_or_404(Loaded_Pl, name=pl_name)
-    pl_list = current_tp.loaded_pl_set.all()
+    current_tp = get_object_or_404(PLTP, name=pltp_name)
+    current_pl = get_object_or_404(PL, name=pl_name)
+    pl_list = current_tp.pl_set.all()
     
     info = json.loads(current_pl.json)
     query = studentCode.objects.filter(student = request.user.username, pl = pl_name)
@@ -41,8 +43,8 @@ def pl_view(request, pltp_name, pl_name):
     return render(request, 'PlayExo/pl.html', dic)
 
 def pltp_view(request, pltp_name):
-    current_tp = get_object_or_404(Loaded_Pltp, name=pltp_name)
-    pl_list = current_tp.loaded_pl_set.all()
+    current_tp = get_object_or_404(PLTP, name=pltp_name)
+    pl_list = current_tp.pl_set.all()
     
     info = current_tp.json
     
@@ -53,11 +55,12 @@ def pltp_view(request, pltp_name):
         "username": request.user.get_full_name(),
     })
 
+@csrf_exempt
 def lti_receiver(request, sha1):
     if (not request.user.is_authenticated):
         return HttpResponse('User not authenticated throught LTI', status=401)
         
-    pltp = get_object_or_404(Loaded_Pltp, sha1=sha1)
+    pltp = get_object_or_404(PLTP, sha1=sha1)
     
     return redirect(pltp_view, pltp.name)
         
