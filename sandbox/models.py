b1c001bdeee45fe759774ec61c690eecea87f647
Dominique <dominique.revuz@gmail.com>
2017-05-29 11:56:22 +0200

    Creation du type Sandbox

--- sandbox/models.py
+++ sandbox/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Sandbox(models.Model):
+    name = models.CharField(max_length=50)
+    url =  models.CharField(max_length=860, null = False)
